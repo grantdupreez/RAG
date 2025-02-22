525f52f63fd12deb84b482c3cdfaed2194ffff4e
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2025-02-22 18:40:28 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ st.title('RAG Chat App')
 
 openai.api_key = st.secrets['auth_key']
 #openai_api_key = st.sidebar.text_input('OpenAI API Key')
-openai_model = st.sidebar.selectbox('AI model', ('gpt-3.5-turbo', 'gpt-4o-mini'),)
-openai_temp = st.sidebar.slider('Temperature', min_value=0.1, max_value=0.8, value=0.2)
-openai_prompt = st.sidebar.text_input('OpenAI prompt')
+#openai_model = st.sidebar.selectbox('AI model', ('gpt-3.5-turbo', 'gpt-4o-mini'),)
+#openai_temp = st.sidebar.slider('Temperature', min_value=0.1, max_value=0.8, value=0.2)
+#openai_prompt = st.sidebar.text_input('OpenAI prompt')
 
 if "messages" not in st.session_state.keys():  # Initialize the chat messages history
     st.session_state.messages = [
